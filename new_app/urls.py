2eabc56ccd4000c995a85ce5c58c9443f338596c
devsr143 <devachandsr143@gmail.com>
2025-11-27 12:23:59 +0530

    new dashboard

--- new_app/urls.py
+++ new_app/urls.py
@@ -5,4 +5,5 @@ from new_app import views
 urlpatterns = [
     path('index/', views.index, name='index'),
     path('dashboard/', views.dashboard, name='dashboard'),
+    path('add_todo/', views.add_todo, name='add_todo'),
 ]
--- new_app/views.py
+++ new_app/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
 
+from new_app.forms import ToDoForm
+
+
 def index(request):
     return render(request, 'index.html')
 def dashboard(request):
@@ -10,3 +13,11 @@ def new(request):
 
 def fresh(request):
     return render(request, 'fresh.html')
+
+def add_todo(request):
+    form = ToDoForm()
+    if request.method == 'POST':
+        form1 = ToDoForm(request.POST)
+        if form1.is_valid():
+            form1.save()
+    return  render(request, 'todo_dash.html', {'form': form})
